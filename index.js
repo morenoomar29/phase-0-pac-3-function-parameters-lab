ef4f946af33b87d4acd08d1d08f466407be421fc
Omar <morenoomar29@yahoo.com>
2021-09-28 16:15:31 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function introduction(name= "Josh") {
+    
+    return `Hi, my name is ${name}.`;
+}
+
+
+function introductionWithLanguage(name, language= "Ember.js") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+
+function introductionWithLanguageOptional(name, language= "JavaScript") {
+    
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
